e25e5916726b6ec191e368ae4f481d16f675088f
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-11 03:12:19 +0100

    Dodaj zasób aws_instance do pliku main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   profile = "default"
   region = "eu-north-1"
 }
+
+
+resource "aws_instance" "app_server" {
+  ami = "ami-007868005aea67c54"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+}
